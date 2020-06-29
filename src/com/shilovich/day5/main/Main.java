9ea7a81971dd28d68608424eaff0fd65fc20f0d5
ThePriest1 <stas.shilovich@gmail.com>
2020-06-29 15:23:39 +0300

    Add some service methods

--- src/com/shilovich/day5/main/Main.java
+++ src/com/shilovich/day5/main/Main.java
@@ -5,8 +5,14 @@ import com.shilovich.day5.service.TextService;
 
 public class Main {
     public static void main(String[] args) {
-        CustomFileReader reader=new CustomFileReader();
-        TextService service=new TextService();
-        service.processingString(reader.readFile(),1,'c');
+        CustomFileReader reader = new CustomFileReader();
+        TextService service = new TextService();
+        String file = reader.readFile();
+        String s1 = service.performTask1ByStringBuilder(file, 6, '|');
+        String s2 = service.performTask2ByStringBuilder(file, 'р', 'а', 'о');
+        String s3 = service.performTask3ByStringBuilder(file, 3, "шо");
+        String s4 = service.performTask4ByStringBuilder(file);
+        String s5 = service.performTask5ByStringBuilder(file, 5);
+        System.out.println(s5);
     }
 }
--- src/com/shilovich/day5/parcer/StringParser.java
+++
@@ -1,8 +0,0 @@
-package com.shilovich.day5.parcer;
-
-public class StringParser {
-    public String[] parse(String string) {
-        String[] result = string.trim().split("\\s+");
-        return result;
-    }
-}
